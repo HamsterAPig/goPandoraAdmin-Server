4e977b42db0bde4b5fa6893abd70a8803fd408b5
HamsterAPig <102395304+HamsterAPig@users.noreply.github.com>
2023-06-21 12:20:39 +0800

    :truck:放到同一个资源的结构体当中

--- model/UserInfoRequest.go
+++
@@ -1,8 +0,0 @@
-package model
-
-type CreateUserInfoRequest struct {
-	Password string  `form:"password" json:"password" binding:"required"`
-	Email    string  `form:"email" json:"email" binding:"required,email"`
-	MFA      *string `form:"mfa" json:"mfa" binding:"omitempty"`
-	Comment  *string `form:"comment" json:"comment" binding:"omitempty"`
-}
--- model/userInfoModel.go
+++ model/userInfoModel.go
@@ -21,3 +21,10 @@ type UserInfo struct {
 	ExpiryTime   time.Time
 	Comment      string
 }
+
+type CreateUserInfoRequest struct {
+	Password string  `form:"password" json:"password" binding:"required"`
+	Email    string  `form:"email" json:"email" binding:"required,email"`
+	MFA      *string `form:"mfa" json:"mfa" binding:"omitempty"`
+	Comment  *string `form:"comment" json:"comment" binding:"omitempty"`
+}
